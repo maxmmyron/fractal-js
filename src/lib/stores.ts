9c6a86e38ebea5471b9e51253f9d54c6fd459408
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-10-27 18:44:18 -0400

    update default store values

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,6 +1,6 @@
 import { writable } from "svelte/store";
 
-export const scale = writable(1.0);
-export const x = writable(0.0);
-export const y = writable(0.0);
+export const scale = writable(0.);
+export const x = writable(0.);
+export const y = writable(0.);
 export const precision = writable(1);
