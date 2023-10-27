056ca6e1a547c7b11c92488d1caaad92554a81e0
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-10-27 16:35:54 -0400

    improve color

--- src/lib/shaders/fragment.glsl
+++ src/lib/shaders/fragment.glsl
@@ -7,21 +7,33 @@ uniform float prec;
 
 varying vec2 vUv;
 
+// from https://gist.github.com/983/e170a24ae8eba2cd174f
+vec3 hsv2rgb(vec3 c) {
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
 int iter(float cx, float cy, int maxIter) {
   float x = 0.;
   float y = 0.;
 
-  float xx = x * x;
-  float yy = y * y;
-  float xy = x * y;
+  float xx = 0.;
+  float yy = 0.;
+  float xy = 0.;
 
   int i = maxIter;
 
-  while ((i-- != 0) && (xx + yy < 10.)) {
+  while ((i-- != 0) && (xx + yy < 4.)) {
+    // calculate products
     xy = x * y;
     xx = x * x;
     yy = y * y;
+
+    // x = x^2 - y^2 + cx
     x = xx - yy + cx;
+
+    // y = 2xy + cy
     y = xy + xy + cy;
   }
 
@@ -33,8 +45,8 @@ void main() {
   float xmax = rightClamp / pow(1.5,scale) - position.x;
 
   float yRange = (xmax - xmin) * (16. / 10.);
-  float ymin = -(yRange - position.y) / 3.4;
-  float ymax = (yRange + position.y) / 3.4;
+  float ymin = -(yRange - position.y) / 3.6;
+  float ymax = (yRange + position.y) / 3.6;
 
   float x = xmin + (xmax - xmin) * vUv.x;
   float y = ymin + (ymax - ymin) * vUv.y;
@@ -42,8 +54,10 @@ void main() {
   int iterCount = 64 * int(prec);
 
   float i = float(iter(x, y, iterCount));
-  float c = i / float(iterCount);
-  vec3 color = vec3(1.-c);
+  float hue = 255. * (i / float(iterCount) / 20.);
+  float val = (i > float(iterCount)) ? 0. : 1.;
+  vec3 hsv = vec3(hue - 0.7, 1., val);
+  vec3 color = hsv2rgb(hsv);
 
   gl_FragColor = vec4(color, 1.);
 }
