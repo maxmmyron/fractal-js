22a5aa3143a67a471a7874a54f8101a01df34717
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-10-29 22:14:29 -0400

    improve prose, UI

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -2,28 +2,31 @@ import { cubicOut } from "svelte/easing";
 import { tweened } from "svelte/motion";
 import { writable } from "svelte/store";
 
+// TODO: enable
+export const isTweenEnabled = writable(true);
+
 export const scale = tweened(0., {
-  duration: 1000,
+  duration: 400,
   easing: cubicOut
 });
 
 export const mn = tweened([-2, -1], {
-  duration: 1000,
+  duration: 400,
   easing: cubicOut
 });
 
 export const mx = tweened([1, 1], {
-  duration: 1000,
+  duration: 400,
   easing: cubicOut
 });
 
 export const cx = tweened(0., {
-  duration: 1000,
+  duration: 400,
   easing: cubicOut
 });
 
 export const cy = tweened(0., {
-  duration: 1000,
+  duration: 400,
   easing: cubicOut
 });
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { scale, mn, mx, cx, cy, exp } from "$lib/stores";
+  import { scale, mn, mx, cx, cy, exp, isTweenEnabled } from "$lib/stores";
   import { Canvas } from "@threlte/core";
   import Scene from "$lib/Scene.svelte";
   import Input from "$lib/components/Input.svelte";
@@ -54,18 +54,18 @@
       <button on:click={reset}>Reset</button>
     </header>
     <fieldset class="border py-2 px-3">
-      <legend>transform</legend>
-      <Input bind:value={$scale} min={0} max={100} step={0.01}>Zoom</Input>
+      <legend><h3 class="text-xl font-semibold">Coloring Options</h3></legend>
+      <Input bind:value={$scale} min={0} max={100} step={0.01}>prec,</Input>
     </fieldset>
     <fieldset class="border py-2 px-3 space-y-3">
-      <legend>render options</legend>
+      <legend><h3 class="text-xl font-semibold">Render Options</h3></legend>
       <select bind:value={viewType} class="py-1 px-2" on:change={reset}>
         <option value="mandelbrot" selected>Mandelbrot Set</option>
         <option value="julia">Julia Set</option>
         <option value="burningship">Burning Ship</option>
       </select>
       {#if viewType === "mandelbrot"}
-        <Input bind:value={$exp} min={2} max={10} step={1}>Exponent</Input>
+        <Input bind:value={$exp} min={2} max={10} step={1}>exp.</Input>
       {/if}
       {#if viewType === "julia"}
         <Input bind:value={$cx} min={-2} max={2} step={0.01}>
@@ -76,6 +76,19 @@
         </Input>
       {/if}
       <br />
+    </fieldset>
+    <fieldset class="border py-2 px-3 space-y-3">
+      <legend><h3 class="text-xl font-semibold">Settings</h3></legend>
+      <div class="grid grid-cols-[2fr,4fr] gap-y-3">
+        <label for="tween-transform">Tween</label>
+        <input
+          id="tween-transform"
+          class="w-4"
+          type="checkbox"
+          disabled
+          bind:checked={$isTweenEnabled}
+        />
+      </div>
     </fieldset>
   </section>
   <section class="flex-1" bind:this={container}>
