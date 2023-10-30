3398e4bf676824a5a6fc040de1dc52b668bbf108
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-10-29 21:36:48 -0400

    add burning ship

--- src/lib/shaders/fragment.glsl
+++ src/lib/shaders/fragment.glsl
@@ -6,7 +6,7 @@ uniform vec2 mx;
 uniform float scale;
 
 uniform vec2 juliaC;
-uniform float view;
+uniform int view;
 
 varying vec2 vUv;
 
@@ -49,7 +49,9 @@ float julia(vec2 c, vec2 p, int maxIter) {
   return float(i) + 1. - log(logzn / log(2.)) / log(2.);
 }
 
-float brot(float cx, float cy, int maxIter) {
+float brot(vec2 c, int maxIter) {
+  vec2 z = vec2(0., 0.);
+
   float x = 0.;
   float y = 0.;
 
@@ -62,15 +64,13 @@ float brot(float cx, float cy, int maxIter) {
   // iterate while the point is within a circle of radius 2
   while ((i-- != 0) && (xx + yy < 4.)) {
     // calculate products
-    xy = x * y;
-    xx = x * x;
-    yy = y * y;
+    xy = z.x * z.y;
+    xx = z.x * z.x;
+    yy = z.y * z.y;
 
-    // x = x^2 - y^2 + cx
-    x = xx - yy + cx;
+    // z = z^2 + c
+    z = vec2(xx - yy + c.x, xy + xy + c.y);
 
-    // y = 2xy + cy
-    y = xy + xy + cy;
   }
 
   if (i < 1) {
@@ -81,22 +81,61 @@ float brot(float cx, float cy, int maxIter) {
   return float(i) + 1. - log(logzn / log(2.)) / log(2.);
 }
 
+float bship(vec2 c, int maxIter) {
+  vec2 z = vec2(0., 0.);
+
+  float xx = 0.;
+  float yy = 0.;
+  float xy = 0.;
+
+  int i = maxIter;
+
+  // iterate while the point is within a circle of radius 2
+  while ((i-- != 0) && (xx + yy < 4.)) {
+    z = vec2(abs(z.x), abs(z.y));
+    // calculate products
+    xy = z.x * z.y;
+    xx = z.x * z.x;
+    yy = z.y * z.y;
+
+    z = vec2(xx - yy + c.x, xy + xy + c.y);
+  }
+
+  if (i < 1) {
+    return 0.;
+  }
+
+  float logzn = log(xx + yy) / 2.;
+  return float(i) + 1. - log(logzn / log(2.)) / log(2.);
+
+
+}
+
 void main() {
   float x = mn.x + (mx.x - mn.x) * vUv.x;
   float y = mn.y + (mx.y - mn.y) * vUv.y;
 
-  int iterCount = 64 + int(pow(2., pow(scale, 0.66)));
+  int iterCount = 256 + int(pow(scale, 2.));
 
   float smoothcol;
-  if(view == 0.) smoothcol = brot(x, y, iterCount);
-  else smoothcol = julia(juliaC, vec2(x, y), iterCount);
+  switch (view) {
+    case 0:
+      smoothcol = brot(vec2(x,y), iterCount);
+      break;
+    case 1:
+      smoothcol = julia(juliaC, vec2(x, y), iterCount);
+      break;
+    case 2:
+      // TODO: is Y flipped?
+      smoothcol = bship(vec2(x, -y), iterCount);
+  }
 
   if(smoothcol == 0.) {
     gl_FragColor = vec4(0.1, 0.1, 0.15, 1.0);
     return;
   }
 
-  vec3 hsv = vec3(smoothcol / 500. - 0.5, 0.8, 0.75);
+  vec3 hsv = vec3(smoothcol / 100. - 0.5, 0.8, 0.75);
   vec3 color = hsv2rgb(hsv);
 
   gl_FragColor = vec4(color, 1.);
